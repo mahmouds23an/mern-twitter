97b738633c58d5fda9b6b794370ab8c5499adace
Mahmoud El-Sakaan <pgemahmoud@gmail.com>
2024-05-03 16:47:59 +0300

    finish user controllers functionality like get user profile, get suggested users, follow or unfollow users and update user profile

---
+++ api/routes/user.routes.js
@@ -0,0 +1,18 @@
+import express from "express";
+import { protectRoute } from "../middleware/protectRoute.js";
+const router = express.Router();
+
+import {
+  getUserProfile,
+  getSuggestedUser,
+  followUnFollowUser,
+  updateUser,
+} from "../controllers/user.controller.js";
+
+// http://localhost:5000/api/users/......
+router.get("/profile/:username", protectRoute, getUserProfile);
+router.get("/suggested", protectRoute, getSuggestedUser);
+router.post("/follow/:id", protectRoute, followUnFollowUser);
+router.post("/update", protectRoute, updateUser);
+
+export default router;
