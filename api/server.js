5c12ec99545eda94ae1a1d463286c5d909853ec1
mahmouds23an <pgemahmoud@gmail.com>
2024-05-09 03:37:52 +0300

    create an image post and payload limit updated in server.js

--- api/server.js
+++ api/server.js
@@ -20,7 +20,7 @@ cloudinary.config({
 const PORT = process.env.PORT || 5000;
 
 // middlewares
-app.use(express.json());
+app.use(express.json({ limit: "7mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
