963d898d26a602dfcb8981475ffc6f48cdf92c06
mahmouds23an <pgemahmoud@gmail.com>
2024-05-09 23:14:37 +0300

    add profile page functionality

--- client/src/components/common/Posts.jsx
+++ client/src/components/common/Posts.jsx
@@ -1,16 +1,19 @@
-/* eslint-disable react-hooks/exhaustive-deps */
 import Post from "./Post";
 import PostSkeleton from "../skeletons/PostSkeleton";
 import { useQuery } from "@tanstack/react-query";
 import { useEffect } from "react";
 
-const Posts = ({ feedType }) => {
+const Posts = ({ feedType, username, userId }) => {
   const getPostEndPoint = () => {
     switch (feedType) {
       case "foyYou":
         return "/api/posts/all";
       case "following":
         return "/api/posts/following";
+      case "posts":
+        return `/api/posts/user/${username}`;
+      case "likes":
+        return `/api/posts/likes/${userId}`;
       default:
         return "/api/posts/all";
     }
@@ -39,7 +42,7 @@ const Posts = ({ feedType }) => {
 
   useEffect(() => {
     refetch();
-  }, [feedType]);
+  }, [feedType, refetch, username]);
 
   return (
     <>
