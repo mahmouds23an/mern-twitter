ca078fc994d035565767f77fa3a1405250119609
mahmouds23an <pgemahmoud@gmail.com>
2024-05-11 02:50:42 +0300

    fix buges in sidebar

--- client/src/components/common/Sidebar.jsx
+++ client/src/components/common/Sidebar.jsx
@@ -7,13 +7,10 @@ import { Link } from "react-router-dom";
 import { BiLogOut } from "react-icons/bi";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import toast from "react-hot-toast";
-import { useState } from "react";
 
 const Sidebar = () => {
   // use it when you need to fetch data
   const queryClient = useQueryClient();
-
-  const [activeLink, setActiveLink] = useState("home");
 
   // use it when you need to manipulate the data like creating, editing and deleting
   const { mutate: logoutMutation } = useMutation({
@@ -44,21 +41,16 @@ const Sidebar = () => {
   return (
     <div className="md:flex-[2_2_0] w-18 max-w-52">
       <div className="sticky top-0 left-0 h-screen flex flex-col border-r border-zinc-500 w-20 md:w-full">
-        <Link
-          to="/"
-          className="flex justify-center md:justify-start"
-          onClick={() => setActiveLink("home")}
-        >
+        <Link to="/" className="flex justify-center md:justify-start">
           <XSvg className="px-2 w-12 h-12 rounded-full fill-white hover:bg-zinc-500" />
         </Link>
         <ul className="flex flex-col gap-3 mt-4">
           <li className="flex justify-center md:justify-start">
             <Link
               to="/"
-              className={`flex gap-3 items-center ${
-                activeLink === "home" ? "bg-white text-black" : ""
-              } hover:bg-zinc-500 transition-all rounded-full duration-300 py-2 pl-2 pr-4 max-w-fit cursor-pointer`}
-              onClick={() => setActiveLink("home")}
+              className={`flex gap-3 items-center 
+              
+               hover:bg-zinc-500 transition-all rounded-full duration-300 py-2 pl-2 pr-4 max-w-fit cursor-pointer`}
             >
               <MdHomeFilled className="w-8 h-8" />
               <span className="text-lg hidden md:block">Home</span>
@@ -67,10 +59,8 @@ const Sidebar = () => {
           <li className="flex justify-center md:justify-start">
             <Link
               to="/notifications"
-              className={`flex gap-3 items-center ${
-                activeLink === "notifications" ? "bg-white text-black" : ""
-              } hover:bg-zinc-500 transition-all rounded-full duration-300 py-2 pl-2 pr-4 max-w-fit cursor-pointer`}
-              onClick={() => setActiveLink("notifications")}
+              className={`flex gap-3 items-center 
+                 hover:bg-zinc-500 transition-all rounded-full duration-300 py-2 pl-2 pr-4 max-w-fit cursor-pointer`}
             >
               <IoNotifications className="w-6 h-6" />
               <span className="text-lg hidden md:block">Notifications</span>
@@ -80,10 +70,7 @@ const Sidebar = () => {
           <li className="flex justify-center md:justify-start">
             <Link
               to={`/profile/${authUser?.username}`}
-              className={`flex gap-3 items-center ${
-                activeLink === "profile" ? "bg-white text-black" : ""
-              } hover:bg-zinc-500 transition-all rounded-full duration-300 py-2 pl-2 pr-4 max-w-fit cursor-pointer`}
-              onClick={() => setActiveLink("profile")}
+              className={`flex gap-3 items-center hover:bg-zinc-500 transition-all rounded-full duration-300 py-2 pl-2 pr-4 max-w-fit cursor-pointer`}
             >
               <FaUser className="w-6 h-6" />
               <span className="text-lg hidden md:block">Profile</span>
@@ -100,17 +87,12 @@ const Sidebar = () => {
                 <img src={authUser?.profileImg || "/avatar-placeholder.png"} />
               </div>
             </div>
-            <div
-              className="flex justify-between flex-1"
-              onClick={() => setActiveLink("profile")}
-            >
+            <div className="flex justify-between flex-1">
               <div className="hidden md:block">
                 <p className="text-white font-bold text-sm w-20 truncate">
                   {authUser?.fullName}
                 </p>
-                <p className="text-slate-500 text-sm">
-                  @{authUser?.username}
-                </p>
+                <p className="text-slate-500 text-sm">@{authUser?.username}</p>
               </div>
               <BiLogOut
                 className="w-5 h-5 cursor-pointer text-slate-700 hover:text-white hover:border hover:rounded"
